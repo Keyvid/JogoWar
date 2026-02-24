b83881897605746e2159adfac8f6c25b15d91d70
Keyvid Lourenço <97493943+Keyvid@users.noreply.github.com>
2026-02-24 21:49:47 +0000

    Implementação do sistema de batalha e atualização automática das tropas.

--- War.c
+++ War.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <locale.h>
 #include <string.h>
+#include <time.h>
 
 // Definição da estrutura para representar um território
 struct Territorio {
@@ -9,8 +10,34 @@ struct Territorio {
     char cor[10];
     int tropas;
 };
+// Realizando batalha e suas regras
+void realizarBatalha(struct Territorio *atacante, struct Territorio *defensor) {
+    printf("\n=== Batalha: %s (Ataque) vs %s (Defesa) ===\n", atacante->nome, defensor->nome);
+    
+    if (atacante->tropas <=1){
+        printf("Ataque CANCELADO! %s possui apenas %d tropa(s).\n", atacante->nome, atacante->tropas);
+    }
+    //Rolagem dos dados
+    int dadoAtaque = (rand() % 6) + 1;
+    int dadoDefesa = (rand() % 6) + 1;
+
+    printf("Ataque tirou: %d\n", dadoAtaque);
+    printf("Defesa tirou: %d\n", dadoDefesa);
+
+    if (dadoAtaque > dadoDefesa) {
+        printf("ATAQUE VENCEU! %ss cede 1 tropa para %s", defensor->nome, atacante->nome);
+        defensor->tropas--;//Perdedor perde 1 tropa
+        atacante->tropas++;//Ganhador ganha 1 tropa
+    } else{
+        printf("A DEFESA VENCEU! %s cede 1 tropa para %s.\n", atacante->nome, defensor->nome);
+        atacante->tropas--;//Perdedor(ataque) perde 1
+        defensor->tropas++;//Ganhador (defesa) ganha 1
+    }
+    
+}
 
 int main() {
+    srand(time(NULL)); //Embaralhar os dados
     struct Territorio
         territorios[5]; // Array para armazenar os territórios
     int i;
@@ -39,8 +66,37 @@ int main() {
         printf("Tropas: %d\n", territorios[i].tropas);
         printf("=========================================\n");
     };
+int continuar = 1;
+int opAtaque, opDefesa;
 
+// Laço repetição da ação BATALHA até usuário digitar 0 para sair
+while (continuar == 1) {
+    printf("\n===========================\n");
+    printf("      MENU DE BATALHA\n");
+    printf("\n===========================\n");
+
+    //Mostrar as opções de territórios atualizadas
+    for (i = 0; i < 5; i++) {
+        printf("[%d] %s (%d tropas)\n", i + 1, territorios[i].nome, territorios[i].tropas);
+    }
     
+    printf("\nDigite o número do território ATACANTE (1 A 5): ");
+    scanf("%d", &opAtaque);
+
+    printf("\nDigite o número do território DEFENSOR (1 A 5): ");
+    scanf("%d", &opDefesa);
+
+    // Validação para jogador não digitar número errado
+    if (opAtaque < 1 || opAtaque > 5 || opDefesa < 1 || opDefesa > 5){
+        printf("Opção inválida! Digite um número válido na quantidade de territórios disponíveis.\n");
+        continue;
+    }
+    realizarBatalha(&territorios[opAtaque - 1], &territorios[opDefesa - 1]);
+
+    printf("\nDeseja realizar outro ataque? (1 para SIM / 0 para NÃO): ");
+    scanf("%d", &continuar);
+}
+printf("\nJogo encerrado!");
     
     return 0;
 }
